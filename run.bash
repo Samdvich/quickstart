5a33ef140a7ff60125a799d80226080429fe0e1c
sammy <samuel.aj.page@gmail.com>
2020-08-30 12:30:17 +1000

    v0.3 - crostini (debian)
    - added flags (--help, --projects)
    - added rootdir (starting the script from any directory # other than the /src file)
    - changed startcd to startdir to match rootdir
    - made script compatible with rootdir, no longer requires to be executed from /quickstart
    - pip (youtube-dl)

--- run.bash
+++ run.bash
@@ -1,43 +1,83 @@
+# Config
+flag=("--help" "--projects")
+desc=("this message" "choose some git projects")
+gits=("https://github.com/Samdvich/quickstart" "https://github.com/Samdvich/CompUI")
+
 # Init
-startcd=$PWD
-touch ~/.bashrc ~/.bash_aliases
-rm ~/{.bashrc,.bash_aliases}
+startdir=$PWD
+rootdir=`find . -name "quickstart"`
+if [[ $rootdir == "" ]]
+then
+	rootdir=.
+fi
+
+# Flags/Optionals
+case $* in
+	"--help")
+	       	echo -e "$(tput bold)sammy's quickstart bash script \U1F495"	
+		for flag in ${flag[@]}
+		do
+			echo "$(tput bold)$(tput setaf 5)$flag $(tput sgr0)${desc[i]}" 
+		done
+	      	exit;;
+	"--projects") # duplicates later operations, a modular approach would probably help fix this
+		echo "$(tput setaf 0)$(tput dim)" && sudo apt-get install -y git && echo "$(tput sgr0)"
+		mkdir -p ~/Code/Projects && cd ~/Code/Projects 
+		for gits in ${gits[@]}
+		do
+			read -p "$(tput setaf 4)$gits $(tput sgr0)(y,n) " ans
+			if [ $ans == "y" ]
+			then
+				gits_ans=(${gits_ans[@]} "${gits[i]}")
+			fi
+		done
+		for i in ${gits_ans[@]}
+		do
+			git clone $i
+		done
+		echo -e
+		cd $startdir;;
+esac
 
 # Terminal/Program Configs
-cp src/.config/{.bashrc,.bash_aliases} ~/
+touch ~/.bashrc ~/.bash_aliases ; rm ~/{.bashrc,.bash_aliases}
+cp $rootdir/src/.config/{.bashrc,.bash_aliases} ~/
 echo "$(tput setaf 1)$(tput bold)- plz add bash_aliases file $(tput sgr0)"
-cd src/.config && cp -RT `ls -a --directory *` ~/.config && cd $startcd
+cd $rootdir/src/.config && cp -RT `ls -a --directory *` ~/.config && cd $startdir
 
 # Code
 cd ~/ && mkdir -p ~/Code/Projects ~/Code/Playground/{Bash,Ruby,Deno,C,}
-cd $startcd
-
+cd $startdir
 
 # Programs
-echo "$(tput setaf 0)$(tput dim)" && sudo apt-get update -y && sudo apt-get full-upgrade -y && sudo apt-get install -y curl vim git flatpak unrar-free && sudo apt autoremove && echo "$(tput sgr0)"
+echo "$(tput setaf 0)$(tput dim)" 
+sudo apt-get update -y && sudo apt-get full-upgrade -y && sudo apt autoremove -y 
+sudo apt-get install -y apt-utils curl vim git flatpak unrar-free python3-pip
+
+sudo pip install --upgrade youtube-dl
+echo "$(tput sgr0)"
 
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 if ! dpkg -s libreoffice7.0
 then
-	wget 'https://download.documentfoundation.org/libreoffice/stable/7.0.0/deb/x86_64/LibreOffice_7.0.0_Linux_x86-64_deb.tar.gz'
-	tar -xf LibreOffice*
-	rm LibreOffice*.tar.gz
-	cd LibreOffice*/DEBS
-	sudo dpkg -i *
-	cd $startcd
-	rm -rf LibreOffice* 
+	wget -P $rootdir 'https://download.documentfoundation.org/libreoffice/stable/7.0.0/deb/x86_64/LibreOffice_7.0.0_Linux_x86-64_deb.tar.gz'
+	tar -xf $rootdir/LibreOffice*.tar.gz
+	rm $rootdir/LibreOffice*.tar.gz
+	sudo dpkg -i $rootdir/LibreOffice*/DEBS/*
+	rm -rf $rootdir/LibreOffice*
+	echo ""
 else
 	echo -e "\n$(tput setaf 6)LibreOffice already installed$(tput sgr0)\n"
 fi
 
 # Fonts
-sudo mkdir -p /usr/share/fonts/{misc,win} && sudo cp -r src/.fonts/misc/* /usr/share/fonts/misc && sudo cp -r src/.fonts/win/* /usr/share/fonts/win ; cd $startcd 
+sudo mkdir -p /usr/share/fonts/{misc,win} && sudo cp -r $rootdir/src/.fonts/misc/* /usr/share/fonts/misc && sudo cp -r $rootdir/src/.fonts/win/* /usr/share/fonts/win ; cd $rootdir 
 
 if ! dpkg -s ttf-mscorefonts-installer
 then
 	wget 'http://ftp.de.debian.org/debian/pool/contrib/m/msttcorefonts/ttf-mscorefonts-installer_3.6_all.deb'
-	if ! sudo dpkg -i ttf-mscorefonts-installer*.deb
+	if ! sudo dpkg -i ttf-mscorefonts-installer*.deb # should run on its own to return the condition
 	then	
 		sudo rm /var/lib/update-notifier/package-data-downloads/partial/*
 		sudo apt-get --purge --reinstall -y install ttf-mscorefonts-installer
@@ -47,3 +87,5 @@ then
 else
 	echo -e "\n$(tput setaf 6)'mscorefonts-installer' package already installed$(tput sgr0)\n"
 fi
+
+cd $startcd
