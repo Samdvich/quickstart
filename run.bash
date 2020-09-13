6a4c768ee71a1e4bb3338329f69a13e7e819459d
sammy <samuel.aj.page@gmail.com>
2020-09-14 09:58:51 +1000

    v0.3.6 - crostini (debian)
    weird non-configured vim indentations fixed, whitespaces are now 4 characters long rather than the 8 or so from before.

--- run.bash
+++ run.bash
@@ -7,38 +7,30 @@ gits=("https://github.com/Samdvich/quickstart" "https://github.com/Samdvich/Comp
 
 # Init
 startdir=$PWD
-rootdir=`find . -name "quickstart"`
-if [[ $rootdir == "" ]]
-then	
-	rootdir=`pwd | sed 's#quickstart.*#quickstart#'`
-fi
+rootdir=`dirname $0` # Returns the script pathname
 
 # Flags/Optionals
 case $* in
-	"--help")
-	       	echo -e "$(tput bold)sammy's quickstart bash script \U1F495"	
-		for flag in ${!flags[@]}
-		do
-			echo "$(tput bold)$(tput setaf 5)${flags[$flag]} $(tput sgr0)${desc[$flag]}" 
-		done
-	      	exit;;
-	"--projects") # duplicates later operations, a modular approach would probably help fix this
-		echo "$(tput setaf 0)$(tput dim)" && sudo apt-get install -y git && echo "$(tput sgr0)"
-		mkdir -p ~/Code/Projects && cd ~/Code/Projects 
-		for git in ${!gits[@]}
-		do
-			read -p "$(tput setaf 4)${gits[$git]} $(tput sgr0)(y,n) " ans
-			if [ $ans == "y" ]
-			then
-				gits_ans=(${gits_ans[@]} "${gits[$git]}")
-			fi
-		done
-		for i in ${gits_ans[@]}
-		do
-			git clone $i
-		done
-		echo -e
-		cd $startdir;;
+    "--help")
+	echo -e "$(tput bold)sammy's quickstart bash script \U1F495"	
+	for flag in ${!flags[@]}; do
+	    echo "$(tput bold)$(tput setaf 5)${flags[$flag]} $(tput sgr0)${desc[$flag]}" 
+	done
+	exit;;
+    "--projects") # duplicates later operations, a modular approach would probably help fix this
+	echo "$(tput setaf 0)$(tput dim)" && sudo apt-get install -y git && echo "$(tput sgr0)"
+	mkdir -p ~/Code/Projects && cd ~/Code/Projects 
+	for git in ${!gits[@]}; do
+	    read -p "$(tput setaf 4)${gits[$git]} $(tput sgr0)(y,n) " ans
+	    if [ $ans == "y" ]; then
+		gits_ans=(${gits_ans[@]} "${gits[$git]}")
+	    fi
+	done
+	for i in ${gits_ans[@]}; do
+	    git clone $i
+	done
+	echo -e
+	cd $startdir;;
 esac
 
 # Terminal/Program Configs
@@ -62,27 +54,27 @@ sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub
 
 if ! [ -f /opt/Discord ]
 then
-	wget -P $rootdir 'https://dl.discordapp.net/apps/linux/0.0.12/discord-0.0.12.tar.gz'
-	sudo tar -xvzf $rootdir/discord*.tar.gz -C /opt
-	rm $rootdir/discord*.tar.gz
-	sudo ln -sf /opt/Discord/Discord /usr/bin/Discord # Symbolic link binary
-	sudo cp $rootdir/src/.config/discord.desktop /usr/share/applications
-	echo ""
+    wget -P $rootdir 'https://dl.discordapp.net/apps/linux/0.0.12/discord-0.0.12.tar.gz'
+    sudo tar -xvzf $rootdir/discord*.tar.gz -C /opt
+    rm $rootdir/discord*.tar.gz
+    sudo ln -sf /opt/Discord/Discord /usr/bin/Discord # Symbolic link binary
+    sudo cp $rootdir/src/.config/discord.desktop /usr/share/applications
+    echo ""
 else
-	cat /usr/share/applications/discord.desktop
-	echo -e "\n$(tput setaf 6)Discord is already installed$(tput sgr0)\n"
+    cat /usr/share/applications/discord.desktop
+    echo -e "\n$(tput setaf 6)Discord is already installed$(tput sgr0)\n"
 fi
 
 if ! dpkg -s libreoffice7.0
 then
-	wget -P $rootdir 'https://download.documentfoundation.org/libreoffice/stable/7.0.0/deb/x86_64/LibreOffice_7.0.0_Linux_x86-64_deb.tar.gz'
-	tar -xf $rootdir/LibreOffice*.tar.gz -C $rootdir
-	rm $rootdir/LibreOffice*.tar.gz
-	sudo dpkg -i $rootdir/LibreOffice*/DEBS/*
-	rm -rf $rootdir/LibreOffice*
-	echo ""
+    wget -P $rootdir 'https://download.documentfoundation.org/libreoffice/stable/7.0.0/deb/x86_64/LibreOffice_7.0.0_Linux_x86-64_deb.tar.gz'
+    tar -xf $rootdir/LibreOffice*.tar.gz -C $rootdir
+    rm $rootdir/LibreOffice*.tar.gz
+    sudo dpkg -i $rootdir/LibreOffice*/DEBS/*
+    rm -rf $rootdir/LibreOffice*
+    echo ""
 else
-	echo -e "\n$(tput setaf 6)LibreOffice already installed$(tput sgr0)\n"
+    echo -e "\n$(tput setaf 6)LibreOffice already installed$(tput sgr0)\n"
 fi
 
 # Fonts
@@ -90,16 +82,15 @@ sudo mkdir -p /usr/share/fonts/{misc,win} && sudo cp -r $rootdir/src/.fonts/misc
 
 if ! dpkg -s ttf-mscorefonts-installer
 then
-	wget 'http://ftp.de.debian.org/debian/pool/contrib/m/msttcorefonts/ttf-mscorefonts-installer_3.6_all.deb'
-	if ! sudo dpkg -i ttf-mscorefonts-installer*.deb # should run on its own to return the condition
-	then	
-		sudo rm /var/lib/update-notifier/package-data-downloads/partial/*
-		sudo apt-get --purge --reinstall -y install ttf-mscorefonts-installer
-		sudo apt --fix-broken -y install
-	fi
-	rm ttf-mscorefonts-installer*.deb
+    wget 'http://ftp.de.debian.org/debian/pool/contrib/m/msttcorefonts/ttf-mscorefonts-installer_3.6_all.deb'
+    if ! sudo dpkg -i ttf-mscorefonts-installer*.deb; then # should run on its own to return the condition
+	sudo rm /var/lib/update-notifier/package-data-downloads/partial/*
+	sudo apt-get --purge --reinstall -y install ttf-mscorefonts-installer
+	sudo apt --fix-broken -y install
+    fi
+    rm ttf-mscorefonts-installer*.deb
 else
-	echo -e "\n$(tput setaf 6)'mscorefonts-installer' package already installed$(tput sgr0)\n"
+    echo -e "\n$(tput setaf 6)'mscorefonts-installer' package already installed$(tput sgr0)\n"
 fi
 
 cd $startcd
--- src/.config/.bash_aliases
+++ src/.config/.bash_aliases
@@ -1,5 +1,5 @@
 # Presets
-alias mp3dl="youtube-dl -x --audio-format mp3 -o '%(title)s.%(ext)'"
+alias mp3dl="youtube-dl -x --audio-format mp3 -o '%(title)s.%(ext)s'"
 
 # Langs
 alias deno"/home/sammy/.deno/bin/deno"
