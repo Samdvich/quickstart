4abe3d5354c91d8063131062df8f860962906c37
sammy <sammy0penguin@gmail.com>
2020-10-17 20:39:19 +1000

    Small refinements
    - Moved code-oss installation candidate
    - Added some small fixes

--- run.bash
+++ run.bash
@@ -19,7 +19,7 @@ case $* in
 	exit;;
     "--projects") # duplicates later operations, a modular approach would probably help fix this
 	echo "$(tput setaf 0)$(tput dim)" && sudo apt-get install -y git && echo "$(tput sgr0)"
-	mkdir -p ~/Code/Projects && cd ~/Code/Projects 
+	mkdir -p ~/Projects/Code && cd ~/Projects/Code
 	for git in ${!gits[@]}; do
 	    read -p "$(tput setaf 4)${gits[$git]} $(tput sgr0)(y,n) " ans
 	    if [ $ans == "y" ]; then
@@ -47,10 +47,12 @@ echo "$(tput setaf 0)$(tput dim)"
 sudo apt-get update -y && sudo apt-get full-upgrade -y && sudo apt autoremove -y 
 sudo apt-get install -y apt-utils neofetch curl vim git flatpak unrar-free python3-pip ffmpeg gimp
 
-sudo pip3 install --upgrade youtube-dl && export PATH=/home/sammy/.local/bin:$PATH
+sudo pip3 install --upgrade youtube-dl && echo PATH=/home/sammy/.local/bin:$PATH >> ~/.bashrc
 echo "$(tput sgr0)"
 
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+flatpak install code-oss
 
 if [ ! -d /usr/local/lib/node_modules ]
 then
@@ -81,7 +83,7 @@ else
     echo -e "\n$(tput setaf 6)Discord is already installed$(tput sgr0)\n"
 fi
 
-if [ ! dpkg -s libreoffice7.0 ]
+if [ ! `dpkg -s libreoffice7.0` ]
 then
     wget -P $rootdir 'https://download.documentfoundation.org/libreoffice/stable/7.0.0/deb/x86_64/LibreOffice_7.0.0_Linux_x86-64_deb.tar.gz'
     tar -xf $rootdir/LibreOffice*.tar.gz -C $rootdir
@@ -93,18 +95,10 @@ else
     echo -e "\n$(tput setaf 6)LibreOffice already installed$(tput sgr0)\n"
 fi
 
-if [ ! dpkg -s code-oss ]
-then
-    curl -s https://packagecloud.io/install/repositories/headmelted/codebuilds/script.deb.sh | sudo bash
-    echo ""
-else
-    echo -e "\n$(tput setaf 6)VSCode (Headmelted) already installed$(tput sgr0)\n"
-fi
-
 # Fonts
 sudo mkdir -p /usr/share/fonts/{misc,win} && sudo cp -r $rootdir/src/.fonts/misc/* /usr/share/fonts/misc && sudo cp -r $rootdir/src/.fonts/win/* /usr/share/fonts/win ; cd $rootdir 
 
-if ! dpkg -s ttf-mscorefonts-installer
+if [ ! `dpkg -s ttf-mscorefonts-installer` ]
 then
     wget 'http://ftp.de.debian.org/debian/pool/contrib/m/msttcorefonts/ttf-mscorefonts-installer_3.6_all.deb'
     if ! sudo dpkg -i ttf-mscorefonts-installer*.deb; then # should run on its own to return the condition
